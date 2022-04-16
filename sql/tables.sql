74fe12ab8c631b0977537fc18b3c9be0b15e6e58
gmartinezarpe <gema.arpe@gmail.com>
2022-04-15 23:43:28 -0400

    Github actions CD

--- sql/tables.sql
+++ sql/tables.sql
@@ -1,19 +1,7 @@
-CREATE TABLE users (
+CREATE TABLE pokemones (
 	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-	first_name VARCHAR(90) NOT NULL,
-	last_name VARCHAR(90) NOT NULL,
-	email VARCHAR(255) NOT NULL
+	pokemon_name VARCHAR(90) NOT NULL,
+	class VARCHAR(255) NOT NULL,
+	foto_pokemon VARCHAR(255)
 );
 
-CREATE TABLE articles (
-	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-	title VARCHAR(2todo5) NOT NULL,
-	content TEXT,
-	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-	user_id INT,
-	KEY user_id_idx(user_id)todo
-);
-
-INSERT INTO users (first_name, last_name, email) VALUES (
-	"gerardo", "martinez", "gerardo.martinez.arpe@ciisa.cl"
-);
